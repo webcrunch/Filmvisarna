26ff2ac9ac773138b6073d6ba7b6ba9f51623e3a
draginojd <arminfazlikhan@gmail.com>
2023-01-26 20:00:10 +0100

    Footer almost done now. only need to add images

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,31 +1,31 @@
 export default function Footer() {
   return <>
     <footer>
-      <div className="adressdiv" >
-      <p className="adresstitle">INFORMATION:</p>
+      <div className="infodiv" >
+      <p className="infotitle">INFORMATION:</p>
       <p className="adress">Stortorget 11, Småstad, 23837</p>
       <p className="adress">Tel: <a href="tel:+4733378901">+47 333 78 901</a></p>
       <p className="email">Email: <a href="#">info@filmvisarna.com</a></p>
      </div>
       
       <div className="openinghdiv">
-        <p className="hoursopentitle">
+        <p className="openinghtitle">
           OPENING HOURS:
+          <br></br><br></br><br></br>
+          
         </p>
+       
         <pre className="hours"> 
         Monday - Friday   : 15:00 - 23:00<br></br>
         Saturday - Sunday : 17:00 - 23:00</pre>
       </div>
 
       <div className="socialsdiv">
-        <p className="cr"> ©Filmvisarna AB</p>
-        <p className="socialstitle">SOCIALS:</p>
-        
-        
-
-      
+        <p className="socialstitle">SOCIALS:
+        <br></br><br></br><br></br> <br></br></p>
+        <p className="socials"> sociala medier bilder</p>
       </div>
-
+      <p className="crfooter">©Filmvisarna AB</p>
 
 
     </footer>
