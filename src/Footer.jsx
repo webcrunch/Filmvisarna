ad62dea4476a64f8441f92a9b44df382ea73b8a7
draginojd <arminfazlikhan@gmail.com>
2023-01-26 15:21:14 +0100

    Footer HTML almost done, only pics left

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,33 @@
 export default function Footer() {
   return <>
     <footer>
+      <div className="adressdiv" >
+      <p className="adresstitle">INFORMATION:</p>
+      <p className="adress">Stortorget 11, Småstad, 23837</p>
+      <p className="adress">Tel: <a href="tel:+4733378901">+47 333 78 901</a></p>
+      <p className="email">Email: <a href="#">info@filmvisarna.com</a></p>
+     </div>
       
-      Här ligger det mycket snygga footern
+      <div className="openinghdiv">
+        <p className="hoursopentitle">
+          OPENING HOURS:
+        </p>
+        <pre className="hours"> 
+        Monday - Friday   : 15:00 - 23:00<br></br>
+        Saturday - Sunday : 17:00 - 23:00</pre>
+      </div>
+
+      <div className="socialsdiv">
+        <p className="cr"> ©Filmvisarna AB</p>
+        <p className="socialstitle">SOCIALS:</p>
+        
+        
+
+      
+      </div>
+
+
+
     </footer>
   </>
 }
