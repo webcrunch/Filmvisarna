ae509873d912967b134a4fcdb1f5c94d53576349
jarl Lindquist <jarllinduist@gmail.com>
2023-01-26 13:18:06 +0100

    adding json fetching and displaying movies

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,5 +1,30 @@
+import { useStates } from "./utilities/states"
+import { useEffect } from 'react';
+
+
 export default function Movies() {
+
+    let s = useStates('movies',{
+    movies: [] });
+
+    useEffect(() => {
+    // Load animal data from /json/niceAnimals.json
+    (async () => {
+      s.movies = await (await fetch("/json/movies.json")).json();
+        })();
+    }, []);
+    
+    
     return <>
-    <p>Movie List</p>
+        <p>Movie List</p>
+        {s.movies.map(
+            movie => <>
+               <div className="movie">
+                    <img src={movie.images[0]} />
+                    <p className="movie">{movie.language} tal</p>
+                    <p>{movie.subtitles} text</p>
+                    </div>
+            </>
+        )}
     </>
 }
