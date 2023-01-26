e3ef1b30000ff2e9c31b77d6343618c4205e3a7c
DueWeb <keviin.sundberg@gmail.com>
2023-01-26 18:06:19 +0100

    To view the web page

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import './utilities/auto-key-lists';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import DetailedInfo from './detailedInfo';
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
   <React.StrictMode>
-    <App />
+    <DetailedInfo />
   </React.StrictMode>
 );
