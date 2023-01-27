33820a58327f0761c455abd40ea84bf9c03beb0a
draginojd <arminfazlikhan@gmail.com>
2023-01-27 14:24:53 +0100

    added img to img folder, HTMLand styling to movies

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -15,16 +15,40 @@ export default function Movies() {
     }, []);
     
     
-    return <>
-        <p>Movie List</p>
+  return<>
+
+       {/*   <p>Movie List</p>
         {s.movies.map(
             movie => <>
                <div className="movie">
-                    <img src={movie.images[0]} />
+                    <img src={movie.image} />
                     <p className="movie">{movie.language} tal</p>
                     <p>{movie.subtitles} text</p>
                     </div>
             </>
-        )}
+        )}  */}
+
+<h1>Filter</h1>
+<div className="imagelistdiv">
+    <hr className="movieshr"></hr>
+    <img className="imagesmovies" src="/images/avatar.jpg" alt="bilden kunde ej visas"></img>
+    <hr className="movieshr"></hr>
+
+    <img className="imagesmovies" src="/images/thewords.jpg" alt="bilden kunde ej visas"></img>
+    <hr className="movieshr"></hr>
+
+    <img className="imagesmovies" src="/images/thelionking.jpg" alt="bilden kunde ej visas"></img>
+    <hr className="movieshr"></hr>
+
+    <img className="imagesmovies" src="/images/thegraveencounters.jpg" alt="bilden kunde ej visas"></img>
+    <hr className="movieshr"></hr>
+    
+    <img className="imagesmovies" src="/images/americanpie2.jpg" alt="bilden kunde ej visas"></img>
+    <hr className="movieshr"></hr>
+</div>
+
+
+
+
     </>
 }
