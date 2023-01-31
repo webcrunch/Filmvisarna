1fec9d31581466b1d7d759a33748a33c10590cd3
draginojd <arminfazlikhan@gmail.com>
2023-01-31 14:34:49 +0100

    fix of image film list

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -11,20 +11,16 @@ export default function Movies() {
 
         <h1>Movie List</h1>
         {/*</>p>filter</p> */}
-        {s.movies.map(
-            movie => <>
-                {
-                    <div className="imagelistdiv">
-                        {/* <hr className="movieshr"></hr> */}
-                        <img className="imagesmovies" src={movie.image} alt="bilden kunde ej visas"></img>
-                        <div className="tidochsalong">
-                            <h2 className="movietitlefilmer">{movie.title}</h2>
-                            <h3 className="tidochsalongtitle">17:30, Salong 1</h3>
-                        </div>
-                        <button className="moviebtnsitplatser" type="submit" value="Submit">Välj sittplatser</button>
-                    </div>
-                }
-            </>
-        )}
-    </div>
+        {s.movies.map(movie => <>
+            <div className="imagelistdiv">
+                {/* <hr className="movieshr"></hr> */}
+                <img className="imagesmovies" src={"../" + movie.images} alt={"Poster av filmen " + movie.title} />
+                <div className="tidochsalong">
+                    <h2 className="movietitlefilmer">{movie.title}</h2>
+                    <h3 className="tidochsalongtitle">17:30, Salong 1</h3>
+                </div>
+                <button className="moviebtnsitplatser" type="submit" value="Submit">Välj sittplatser</button>
+            </div>
+        </>)}
+    </div >
 }
