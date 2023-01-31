ebbd8105b9ebc005599f053695965c25f997175a
draginojd <arminfazlikhan@gmail.com>
2023-01-31 17:59:41 +0100

    starting on the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,31 @@
 export default function Home() {
-    return <>
-    
-    </>
+  return <>
+
+
+
+   
+  <div className="hometopcontainer">
+<div className="hometopimagescontainer">
+  <img src="/images/elephant.jpg" alt="elepfat bild" width="150px"  height="200px"></img>
+</div>
+<div className="hometopimagescontainer">
+  <img src="/images/elephant.jpg" alt="elepfat bild" width="150px"  height="200px"></img>
+</div>
+<div className="hometopimagescontainer">
+  <img src="/images/elephant.jpg" alt="elepfat bild" width="150px"  height="200px"></img>
+</div>
+<div className="hometopimagescontainer">
+  <img src="/images/elephant.jpg" alt="elepfat bild" width="150px"  height="200px"></img>
+</div>
+<div className="hometopimagescontainer">
+  <img src="/images/elephant.jpg" alt="elepfat bild" width="150px"  height="200px"></img>
+</div>
+
+  </div>
+   
+      
+
+
+      
+</>
 }
--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -1,3 +1,4 @@
 export default function Movies() {
     return <h1> An movies page that will handle things.</h1>
 }
+
