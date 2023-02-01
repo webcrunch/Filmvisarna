fd5ce832c812e8197329448fd45afbf61912ae26
draginojd <arminfazlikhan@gmail.com>
2023-02-01 11:23:16 +0100

    top html done for home page, added css also

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,17 +5,20 @@ export default function Home() {
    
   
 <div className="flex-container">
-  <div><img className="topcontainerimages" src="/images/avatar.jpg" 
-   width="275px"></img></div>
-  <div><img className="topcontainerimages" src="/images/blackpanter.jpg" 
-   width="275px"></img></div>
-  <div><img className="topcontainerimages" src="/images/amancalledotto.jpg" 
-   width="275px"></img></div>  
-      <div><img className="topcontainerimages" src="/images/burnallmyletters.jpg" 
-   width="275px"></img> <button></button></div>
-  <div><img className="topcontainerimages" src="/images/themenu.jpg" 
-   width="275px"></img></div>
-</div>
+      <div><a href="#"><img className="topcontainerimages" src="/images/avatar.jpg"
+        width="100px"></img></a><p>Avatar: The Way Of Water</p></div>
+      <div><a href="#"><img className="topcontainerimages" src="/images/blackpanter.jpg"
+        width="100px"></img></a><p>Forever Wakanda</p></div>
+      <div><a href="#"><img className="topcontainerimages" src="/images/amancalledotto.jpg"
+        width="100px"></img></a><p>En Man Som Heter Ove</p></div>  
+      <div><a href="#"><img className="topcontainerimages" src="/images/burnallmyletters.jpg"
+        width="100px"></img></a><p>Bränn Alla Mina Brev</p></div>
+      <div><a href="#"><img className="topcontainerimages" src="/images/themenu.jpg"
+        width="100px"></img></a><p>The Menu</p></div>
+    </div>
+    
+
+
 
    
       
