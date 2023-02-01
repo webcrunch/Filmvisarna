c71ee760cce10c0c44823154b7abc48cd577b784
draginojd <arminfazlikhan@gmail.com>
2023-02-01 12:00:52 +0100

    added title and border radius

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,31 +2,31 @@ export default function Home() {
   return <>
 
 
-   
-  
-<div className="flex-container">
+
+
+    <div className="flex-container">
       <div><a href="#"><img className="topcontainerimages" src="/images/avatar.jpg"
         width="100px"></img></a><p>Avatar: The Way Of Water</p></div>
       <div><a href="#"><img className="topcontainerimages" src="/images/blackpanter.jpg"
         width="100px"></img></a><p>Forever Wakanda</p></div>
       <div><a href="#"><img className="topcontainerimages" src="/images/amancalledotto.jpg"
-        width="100px"></img></a><p>En Man Som Heter Ove</p></div>  
+        width="100px"></img></a><p>En Man Som Heter Ove</p></div>
       <div><a href="#"><img className="topcontainerimages" src="/images/burnallmyletters.jpg"
         width="100px"></img></a><p>Bränn Alla Mina Brev</p></div>
       <div><a href="#"><img className="topcontainerimages" src="/images/themenu.jpg"
         width="100px"></img></a><p>The Menu</p></div>
     </div>
-    
+
 
     <div className="hottestrelease">
-      
-    <img className="hottestreleasemovie" src="/images/avatarbig.jpg"></img>
-</div>
-
-   
-      
+      <p>Hetaste just nu</p>
+      <img className="hottestreleasemovie" src="/images/avatarbig.jpg"></img>
+    </div>
 
 
-      
-</>
+
+
+
+
+  </>
 }
