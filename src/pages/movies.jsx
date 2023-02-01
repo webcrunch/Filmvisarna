e2886e22f25388c71e14dfc6a97c16b229d72cdd
harisha7 <harisha.srinivas74@gmail.com>
2023-02-01 10:30:18 +0100

    ading contact information to the page

---
+++ src/pages/movies.jsx
@@ -0,0 +1,29 @@
+import { useStates } from "../utilities/states";
+import { useEffect } from 'react';
+
+
+export default function Movies() {
+
+    let s = useStates('main');
+
+
+    return <div className="movieList">
+
+        <h1>Movie List</h1>
+        {/*</>p>filter</p> */}
+        {s.movies.map(movie => <>
+            <div className="imagelistdiv">
+                {/* <hr className="movieshr"></hr> */}
+                <img className="imagesmovies" src={"../" + movie.images} alt={"Poster av filmen " + movie.title} />
+                <div className="tidochsalong">
+                    <h2 className="movietitlefilmer">{movie.title}</h2>
+                    <h3 className="tidochsalongtitle">17:30, Salong 1</h3>
+                </div>
+                <button className="moviebtnsitplatser" type="submit" value="Submit">Välj sittplatser</button>
+            </div>
+        </>)}
+    </div >
+
+    
+
+}
