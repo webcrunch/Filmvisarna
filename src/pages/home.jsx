fa9e90269bf64d3b995d540d1c7b4b0df6820242
draginojd <arminfazlikhan@gmail.com>
2023-02-01 09:46:38 +0100

    changed the pics

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,13 +5,13 @@ export default function Home() {
    
   
 <div className="flex-container">
-  <div><img className="topcontainerimages" src="/images/elephant.jpg" 
+  <div><img className="topcontainerimages" src="/images/avatar.jpg" 
    width="275px"></img></div>
-  <div><img className="topcontainerimages" src="/images/elephant.jpg" 
+  <div><img className="topcontainerimages" src="/images/blackpanter.jpg" 
    width="275px"></img></div>
-  <div><img className="topcontainerimages" src="/images/elephant.jpg" 
+  <div><img className="topcontainerimages" src="/images/burnallmyletters.jpg" 
    width="275px"></img></div>  
-  <div><img className="topcontainerimages" src="/images/elephant.jpg" 
+  <div><img className="topcontainerimages" src="/images/.jpg" 
    width="275px"></img> <button></button></div>
   <div><img className="topcontainerimages" src="/images/elephant.jpg" 
    width="275px"></img></div>
