7e53be7f59102d2ba40ed0625fc84ccf657e9fca
Vanishah30 <vanisha.shah@gmail.com>
2023-02-02 12:42:14 +0100

    changes in OmOss style

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ export default function About() {
             <h2>Om Oss</h2>
             <p>Vårt varumärkeslöfte, We Make Movies Better, innebär att vi alltid strävar efter att erbjuda de mest minnesvärda och omvälvande  upplevelserna i biografer i världsklass.</p>
 
-            <p>Vi är stolta över att göra det genom ett förstklassigt bemötande från vår personal, stor duk, bekväma VIP-fåtöljer med maximal komfort tillsammans med den bästa tekniken för ljud och bild.Dessutom utvecklar vi löpande våra digitala plattformar där man enkelt kan finna inspiration och information.</p>
+            <p>Vi är stolta över att göra det genom ett förstklassigt bemötande från vår personal,stor duk, bekväma VIP-fåtöljer med maximal komfort tillsammans med den bästa tekniken för ljud och bild.Dessutom utvecklar vi löpande våra digitala plattformar där man enkelt kan finna inspiration och information.</p>
 
             <p>Filmvisarna som arbetsgivare erbjuder sina medarbetare karriärsmöjligheter och en arbetsmiljö som inspirerar. Vi är övertygade om att en förutsättning för att höja besöket på våra biografer är ett team som känner passion för film och brinner för att leverera den bästa möjliga upplevelsen till våra gäster.</p>
 
