9582a33e7beee98bc4fc5004af78e715a9db037a
jarl Lindquist <jarllinduist@gmail.com>
2023-02-02 21:58:13 +0100

    new programmatic function for the film/screening list

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -1,11 +1,11 @@
 import { useStates } from "../utilities/states";
-// import { calculatingTime } from "../utilities/length-calculating";
+import { calculatingTime } from "../utilities/length-calculating";
 export default function Movies() {
     let { movies, screening } = useStates('main');
     
     function getMovies(name) {
-        let { images } = movies.find(movie => movie.title === name);
-        return images;
+        let { images,length } = movies.find(movie => movie.title === name);
+        return [images,length];
     }
     
     return <div className="movieList">
@@ -14,13 +14,12 @@ export default function Movies() {
         {screening.map(display => <>
             <div className="imagelistdiv">
                 {/* <hr className="movieshr"></hr> */}
-                <img className="imagesmovies" src={"../" + getMovies(display.film)} alt={"Poster av filmen " + display.film} />
+                <img className="imagesmovies" src={"../" + getMovies(display.film)[0]} alt={"Poster av filmen " + display.film} />
                  
                 <div className="tidochsalong">
                     <h2 className="movietitlefilmer">{display.film}</h2>
-                    <h3 className="tidochsalongtitle">{display.auditorium}</h3>
-                    <h3 className="tidochsalongtitle">{display.date} : {display.time}</h3>
-                    <h4 className="tidochsalongtitle">{calculatingTime(movie.length)}</h4>
+                    <h4 className="tidochsalongtitle">Sal: {display.auditorium}. Dag: {display.date} </h4>
+                    <h4 className="tidochsalongtitle">Tid: {display.time}. Längd: {calculatingTime(getMovies(display.film)[1])}</h4>
                 </div>
                 <button className="moviebtnsitplatser" type="submit" value="Submit">Välj sittplatser</button>
             </div>
