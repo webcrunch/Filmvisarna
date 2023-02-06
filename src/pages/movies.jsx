0d1378e43252a5fb46c263be5ed8d0f613c69180
jarl Lindquist <jarllinduist@gmail.com>
2023-02-06 16:41:45 +0100

    text fix for saloons and adding filter for film list(Still a prototype)

--- src/pages/movies.jsx
+++ src/pages/movies.jsx
@@ -1,12 +1,42 @@
 import { useStates } from "../utilities/states";
+import { calculatingTime } from "../utilities/length-calculating";
 
 export default function Movies() {
-    let {movies, screening} = useStates('main');
-    return <div className="movieList">
+    let s = useStates('main');
+    let movieTitles = s.movies.map(movie => movie.title);
+    
+    function getMovies(name) {
+        let { images,length } = s.movies.find(movie => movie.title === name);
+        return [images,length];
+    }
 
+    function filterMovie(film) {
+        s.screening = s.screening.filter(movie => movie.film === film);
+    }
+
+    function filterSallons(auditorium) {
+        s.screening = s.screening.filter(movie => movie.auditorium === auditorium);
+    }
+    
+    return <div className="movieList">
         <h1>Movie List</h1>
+        <p>Salonger:</p>
+        <select onChange={e => filterSallons(e.target.value)} name="selectList" id="selectList">
+            <optgroup label="Movies">
+                <option value="All">All</option>
+                <option value="Stora Salongen">Stora Salongen</option>
+                <option value="Lilla SAlongen">Lilla Salongen</option>
+                </optgroup>
+        </select>
+        <p>Movies:</p>
+        <select onChange={e => filterMovie(e.target.value)} name="selectList" id="selectList">
+            <optgroup label="Movies">
+                <option value="All">All</option>
+            {movieTitles.map(title => <option value={title}>{title}</option> )}
+                </optgroup>
+        </select>
         {/*</>p>filter</p> */}
-        {movies.map(movie => <>
+        {s.screening.map(display => <>
             <div className="imagelistdiv">
                 {/* <hr className="movieshr"></hr> */}
                 <img className="imagesmovies" src={"../" + movie.images} alt={"Poster av filmen " + movie.title} />
