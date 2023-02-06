3d4c105af4e5ae56f7ff6899e4d7132d43f498be
jarl Lindquist <jarllinduist@gmail.com>
2023-02-06 17:59:00 +0100

    getting information from the usestates for the current movie for details page

--- src/DetailedInfo.jsx
+++ src/DetailedInfo.jsx
@@ -1,60 +1,19 @@
+import { useStates } from './utilities/states';
+import { useParams } from 'react-router-dom';
+import { calculatingTime } from "../utilities/length-calculating";
+
 export default function DetailedInfo() {
-    const movie = {
-        "title": "Call me by your name",
-        "productionCountries": [
-            "Italien",
-            "USA"
-        ],
-        "productionYear": 2017,
-        "length": 132,
-        "genre": "Drama",
-        "distributor": "UIP",
-        "language": "engelska",
-        "subtitles": "svenska",
-        "director": "Luca Guadagnino",
-        "actors": [
-            "Armie Hammer  ",
-            "Timothée Chalamet  ",
-            "Michael Stuhlbarg  "
-        ],
-        "description": "Filmen utspelas i norra Italien sommaren 1983. En ung amerikansk-italienare blir förälskad i en amerikansk student som kommer för att studera och bo hos hans familj.Tillsammans upplever de en oförglömlig sommar - full av musik, mat och kärlek - som för evigt kommer att förändra dem.",
-        "images": [
-            "https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_.jpg",
-            "https://m.media-amazon.com/images/M/MV5BMzBlMWQ4MDMtMWEzNy00MWJiLWI2ZDgtZjBkOTkwMTFlOTNhXkEyXkFqcGdeQTNwaW5nZXN0._V1_.jpg",
-            "ttps://static.metacritic.com/images/products/movies/6/33ef47117e90c768a4632c77ed16da3e-2400.jpg",
-            "call-me-poster1.jpg",
-            "call-me-poster2.jpg"
-        ],
-        "youtubeTrailers": [
-            "Z9AYPxH5NTM"
-        ],
-        "trailer": "https://www.youtube.com/watch?v=Z9AYPxH5NTM?autoplay=1&mute=1",
-        "reviews": [
-            {
-                "source": "Sydsvenskan",
-                "quote": "ett drama berättat med stor ömhet",
-                "stars": 4,
-                "max": 5
-            },
-            {
-                "source": "Svenska Dagbladet",
-                "quote": "en film att förälska sig i",
-                "stars": 5,
-                "max": 5
-            },
-            {
-                "source": "DN",
-                "quote": "en het romans i åttiotalskostym",
-                "stars": 4,
-                "max": 5
-            }
-        ]
-    }
-    return <>
+        const { moviePath } = useParams();
+    
+        const s = useStates('main');
+    const movie = s.movies.find(movie => movie.title == moviePath);
+     
+     return <>
+        {
+            movie != undefined ? 
         <div className="detailedPageContainer">
             <div className="leftContainerImgTrailer">
-                <img className="detailedImages" src={movie.images} />
-                <iframe className="detailedTrailer" src="https://www.youtube.com/embed/Z9AYPxH5NTM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <img className="detailedImages" src={movie.images} />    
             </div>
 
             <h1 className="detailedTitle">{movie.title}</h1>
@@ -63,7 +22,7 @@ export default function DetailedInfo() {
                     <p className="detailedDesc">{movie.description}</p> <div />
                 </div>
                 <div className="detailedInfo">
-                    <p className="detailedLength">Length: {movie.length}</p>
+                    <p className="detailedLength">Length: {calculatingTime(movie.length)}</p>
                     <p className="detailedGenre">Genre: {movie.genre}</p>
                     <p className="detailedReleaseDate">Released: {movie.productionYear}</p>
                     <p className="detailedDirector">Director: {movie.director}</p>
@@ -86,9 +45,8 @@ export default function DetailedInfo() {
                 )}</p>
             </div>
             <button onClick={() => alert('Booking coming soon')}>Book Now</button>
-
-        </div>
-
+            </div>
+        : null}
     </>
 }
 
