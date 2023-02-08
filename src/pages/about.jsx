5fa02b14a8831e44ff377d9b61efe1d8f78eeed8
zompes <simonisvedala@hotmail.com>
2023-02-08 15:38:00 +0100

    adding more information in about us page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,17 +3,28 @@ export default function About() {
         <div className="OmOss">
             <h2>Om Oss</h2>
             <img src="images/Om.jpg" alt="Here will be a image" />
-            <p>Vårt varumärkeslöfte, <b>We Make Movies Better</b>, innebär att vi alltid strävar efter att erbjuda de mest minnesvärda och omvälvande  upplevelserna i biografer i världsklass.</p>
+            <p>Vårt varumärkeslöfte, <b>We Make Movies Better</b>,
+                innebär att vi alltid strävar efter att erbjuda de mest minnesvärda och omvälvande  upplevelserna i biografer i världsklass.</p>
 
-            <p>Vi är stolta över att göra det genom ett förstklassigt bemötande från vår personal, stor duk, bekväma VIP-fåtöljer med maximal komfort tillsammans med den bästa tekniken för ljud och bild. Dessutom utvecklar vi löpande våra digitala plattformar där man enkelt kan finna inspiration och information.</p>
+            <p>Vi är stolta över att göra det genom ett förstklassigt bemötande från vår personal,
+                stor duk, bekväma VIP-fåtöljer med maximal komfort tillsammans med den bästa tekniken för ljud och bild
+                genom IMAX bild och Prophons ljud biosystem levererar vi den bästa bio upplevensen i världen. Båda våra salonger har det bästa av det bästa i allt,
+                dessutom utvecklar vi löpande våra digitala plattformar där man enkelt kan finna inspiration och information.</p>
 
-            <p>Filmvisarna som arbetsgivare erbjuder sina medarbetare karriärsmöjligheter och en arbetsmiljö som inspirerar. Vi är övertygade om att en förutsättning för att höja besöket på våra biografer är ett team som känner passion för film och brinner för att leverera den bästa möjliga upplevelsen till våra gäster.</p>
+            <p>Filmvisarna som arbetsgivare erbjuder sina medarbetare karriärsmöjligheter och en arbetsmiljö som inspirerar.
+                Vi är övertygade om att en förutsättning för att höja besöket på våra biografer är ett team som känner
+                passion för film och brinner för att leverera den bästa möjliga upplevelsen till våra gäster.</p>
 
-            <p>Biografen öppnar alltid 30 minuter innan första föreställning. Öppettiderna kan ändras vid specialevenemang, helgdagar eller skollov.</p>
+            <p>Vår biosalong kan hittas på denna adress Stortorget 11, 124 84 Småstad, Sverige, behöver du kontakta oss kan du ringa telefonnumret
+                (073) 337 89 01 eller skicka ett email till info@filmvisarna.com. Biografen öppnar alltid 30 minuter innan första föreställning.
+                Öppettiderna kan ändras vid specialevenemang, helgdagar eller skollov.</p>
 
             <p>Midsommarafton och julafton har vi stängt, men dagen efter står vi alltid redo att välkomna biobesökare.</p>
 
-            <p>Filmvisarna tillåter inte att man tar med egen dryck, mat, snacks, godis, popcorn osv in i biografen. Vi har ett stort utbud av dryck, mat, snacks, popcorn och godis i våra butiker, för att alla ska kunna hitta något som passar dem. Det är tillåtet att ta med egen snacks/dryck om du har hälsomässiga skäl för det</p>
+            <p>Filmvisarna tillåter inte att man tar med egen dryck, mat, snacks, godis, popcorn osv in i biografen.
+                Mat, dryck, snacks och godis kan köpas från receptionen, det kommer med stora mängder tack vare vår
+                partnerskap med Snacksjätten. Det finns något för alla, det är tillåtet att ta med egen snacks/dryck om
+                du har hälsomässiga skäl för det</p>
         </div>
     </>;
 }
