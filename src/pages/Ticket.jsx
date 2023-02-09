bdc213acc6b4c7c69081810ad45af44063a9d5fd
jarl Lindquist <jarllinduist@gmail.com>
2023-02-09 13:12:26 +0100

    add a page for furter work on booking page

---
+++ src/pages/Ticket.jsx
@@ -0,0 +1,21 @@
+import { useStates } from '../utilities/states';
+import { useParams,Link } from 'react-router-dom';
+import { calculatingTime } from '../utilities/length-calculating';
+
+
+export default function TicketPage() { 
+     const { moviePath } = useParams();
+    
+        const s = useStates('main');
+    const movie = s.movies.find(movie => movie.title == moviePath);
+    
+    
+    return <>
+        
+        {
+            movie != undefined ? <div className="detailedPageContainer">
+                            <h1  className="detailedTitle">Boka platser för filmen: {movie.title}</h1>
+            </div>: null
+    }
+    </>
+}
