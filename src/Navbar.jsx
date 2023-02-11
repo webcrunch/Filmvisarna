e47602501ce08d9eb18dc6b1c4af0363bea12a59
jarl Lindquist <jarllinduist@gmail.com>
2023-02-11 11:33:06 +0100

    WIP handling login icon and modals for redirect to other page

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,18 @@
 import { useStates } from './utilities/states';
 import { NavLink } from "react-router-dom"
+import { useState } from 'react';
 export default function Navbar() {
 
     const s = useStates('main');
-
+    const [isOpen, setIsOpen] = useState(false);
     return <>
-        <nav className="navbar">
-            <nav>
+        <nav>
                 {s.menu.map(({ label, path }) =>
                     label ? <NavLink className="navlinks" to={path}>{label}</NavLink> : null
-                )}
-            </nav>
+            )}
+            <img onClick={() => alert("hello")} src="/images/icon-login.png" alt="Login" />
+            {/* <p>Välkommen tillbaka ....</p> */}            
         </nav>
-
+        
     </>
 }
