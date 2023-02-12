de79b6691894d61accf6fef271f90f033ade6445
jarl Lindquist <jarllinduist@gmail.com>
2023-02-12 12:22:00 +0100

    adding a copy function and call it to copy the booking number

--- src/pages/booking-confirmatin.jsx
+++ src/pages/booking-confirmatin.jsx
@@ -1,36 +1,62 @@
+import { useStates } from "../utilities/states";
+import copyContent from '../utilities/copyFunction';
+import { calculatingTime } from '../utilities/length-calculating';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 export default function Booked() {
+    const { bookingId } = useParams();
+    const { movies, screenings } = useStates('main');
+    const l = useStates({
+        movie: null,
+        screning: null
+    });
+    
+    useEffect(() => {
+        l.movie = movies.find(movie => movie.path === bookingId);
+        l.screning = l.movie ? screenings.find(screen => screen.film === l.movie.title) : null;
+    }, []);
+        
+
     let of = {
         phoneNumber: 333333,
         numberOfChildren: 0,
         numberOfAdults: 4,
         numberOfSeniors: 2,
+        totalPris: 140,
         confirmationNumber: "1213344554",
         calculateAll() {
             return this.numberOfAdults + this.numberOfChildren + this.numberOfSeniors
         }
     }
 
-
-    return <div className="doneA">
+    return <>{ l.movie && l.screning ? 
+        < div className = "doneA" >
 
         <div className="container">
             <div className="image">
-                <img src="/images/bookingpage.jpg" alt="Here will be a image" />
+                <img src="/images/bookingpage.jpg" alt="En bild över popcorn och andra roliga saker" />
             </div>
         </div>
 
 
         <h2>Thank you for your order!</h2>
-        <h3>Info</h3>
-        <p>The Movie:{ }</p>
-        <p>Your email:<br />{of.email}</p>
-        <p>Your phone number:<br />{of.phoneNumber}</p>
-        <p>Number of child tickets.<br />{of.numberOfChildren}</p>
-        <p>Number of adult tickets.<br />{of.numberOfAdults}</p>
-        <p>Number of senior tickets.<br />{of.numberOfSeniors}</p>
-        <p>Total: {of.calculateAll()}</p>
-        <h3>Booking confirmation</h3>
-        <p>Don't forget to bring this number to the cinema:</p>
+
+        <h3>Information</h3>
+        <h4>Film: {l.movie.title}</h4>
+        <p>Salong: {l.screning.auditorium}</p>
+        <p>Datum: {l.screning.date}</p>
+        <p>Tid: {l.screning.time}</p>
+    {/* <p>Email:<br />{of.email}</p> */ }
+        <p>telefonnummer:<br />{of.phoneNumber}</p>
+        <p>Antal barn biljetter:<br />{of.numberOfChildren}</p>
+        <p>Antal vuxen biljetter:<br />{of.numberOfAdults}</p>
+        <p>Antal pensionär biljetter:<br />{of.numberOfSeniors}</p>
+        <p>Total antal biljetter: {of.calculateAll()}</p>
+        <p>Pris: {of.totalPris }kr</p>
+        <h3>Boknings information</h3>
+        <p>Glöm inte att ta med bokningsnummret till biografen:</p>
         <p>{of.confirmationNumber}</p>
-    </div>
+        <button type="button" onClick={()=> copyContent(of.confirmationNumber)}>Kopiera bokningsnummret</button>
+    </div > : null }</>
 }
