d0878741358a1c9a3ef8fbfbaee595e35deaa6ec
draginojd <arminfazlikhan@gmail.com>
2023-02-12 14:35:36 +0100

    ticket quantity selector almost done

--- src/pages/Ticket.jsx
+++ src/pages/Ticket.jsx
@@ -10,7 +10,42 @@ export default function TicketPage() {
     const movie = s.movies.find(movie => movie.title == moviePath);
     
     
-return <>
+/* 
+    const ticketOptions = document.querySelectorAll(".ticket-option1,.ticket-option2, .ticket-option3") ;
+    const totalPriceDisplay = document.querySelector("#total-price");
+    
+    ticketOptions.forEach(option => {
+      const addButton = option.querySelector(".add-ticket");
+      const removeButton = option.querySelector(".remove-ticket");
+      const ticketCountInput = option.querySelector("input[type='text']");
+    
+      addButton.addEventListener("click", function() {
+        ticketCountInput.value = parseInt(ticketCountInput.value) + 1;
+        updateTotalPrice();
+      });
+    
+      removeButton.addEventListener("click", function() {
+        if (ticketCountInput.value > 0) {
+          ticketCountInput.value = parseInt(ticketCountInput.value) - 1;
+          updateTotalPrice();
+        }
+      });
+    });
+    
+    function updateTotalPrice() {
+        let totalPrice = 0;
+        
+        ticketOptions.forEach(option => {
+          const ticketCount = parseInt(option.querySelector("input[type='text']").value);
+          const ticketPriceString = option.querySelector("p").textContent.split("Kr")[0];
+          const ticketPrice = parseInt(ticketPriceString.split(":")[1].trim());
+          totalPrice += ticketCount * ticketPrice;
+        });
+        
+        totalPriceDisplay.textContent = `Total Pris: ${totalPrice} Kr`;
+      }  */
+    
+return <div className="full-ticket-page">
         
        {/*  {
             movie != undefined ? <div className="detailedPageContainer">
@@ -42,5 +77,8 @@ return <>
         <p className="total-price-css" id="total-price">Total Pris: 0Kr</p>
     </div>
 
-</>
+</div>
+
+
+
 }
