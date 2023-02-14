20822e7ccf134d9af56fbd378eb1bb6c40510712
jarl Lindquist <jarllinduist@gmail.com>
2023-02-14 14:43:34 +0100

    adding animation to the box

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,14 +10,14 @@ export default function Navbar() {
                 {s.menu.map(({ label, path }) =>
                     label ? <NavLink className="navlinks" to={path}>{label}</NavLink> : null
             )}
-            <img onClick={() => open.isOpened = !open.isOpened} src="/images/icon-login.svg" alt="Login" />
+            <img className='img' onClick={() => open.isOpened = !open.isOpened} src="/images/icon-login.svg" alt="Login" />
             {/* <p>Välkommen tillbaka ....</p> */}            
-        </nav>
         {open.isOpened ? <div className="boxContent">
             <div>
-            <p>{open.loggedin ? <Link className='linkin-park' to={"#"}><img  className='linkin-park' src={"./images/icons8-log-out-25.png"} alt={"Poster av filmen "} />  Log out</Link> : <Link to={"#"}><img  src={"./images/icons8-log-in-25.png"} alt={"Poster av filmen "} />Log in</Link>}</p>
-            <p><Link className='linkin-park' to={"#"}><img className='linkin-park' src={"./images/icons8-add-user-male-24.png"} alt={"Poster av filmen "} />  Registrera sig </Link></p>
+            <p>{open.loggedin ? <Link  to={"#"}><img   src={"./images/icons8-log-out-25.png"} alt={""} /><p>Log out</p></Link> : <Link to={"#"}><img  src={"./images/icons8-log-in-25.png"} alt={""} />Log in</Link>}</p>
+            <p><Link  to={"#"}><img  src={"./images/icons8-add-user-male-24.png"} alt={""} />  Registrera sig </Link></p>
             </div>
-        </div> :null}
+        </div> :<div className="notShowMe"></div>}
+        </nav>
     </>
 }
