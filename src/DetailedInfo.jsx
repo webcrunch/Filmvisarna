4461ed7e6faed21e54b3a1791de75fc0a592a2c7
Kevin Sundberg <Kevin@Kevins-MacBook-Air.local>
2023-02-15 13:37:21 +0100

    working screening on detail page

--- src/DetailedInfo.jsx
+++ src/DetailedInfo.jsx
@@ -1,15 +1,24 @@
 import { useStates } from './utilities/states';
 import { useParams, Link } from 'react-router-dom';
 import { calculatingTime } from './utilities/length-calculating';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import Trailer from ''
 export default function DetailedInfo() {
     const { moviePath } = useParams();
 
     const s = useStates('main');
     const movie = s.movies.find(movie => movie.title == moviePath);
-    const [isOpen, setIsOpen] = useState(true);
+    const [showRatings, setShowRatings] = useState(false);
+    const [showScreenings, setShowScreenings] = useState(false);
     const trailer = movie && movie.youtubeTrailer;
+    const screenings = s.screening.filter(screen => screen.film == movie.title); 
+    const testBlock = screenings.filter((screen,index) => {
+        if(index < 4) return screen;
+    })
+
+    useEffect(() => {
+        console.log('check the context of s ', screenings);
+    },[])
 
     return <>
         {
@@ -18,21 +27,20 @@ export default function DetailedInfo() {
                     <div className='detailedLeftContainer'>
                         <img className="detailedImages" src={movie.images} />
                         {/* <Trailer className="someting" embedId="xjDjIWPwcPU" /> */}
-                        <iframe className="movieTrailer" width="350px" height="315px" src={"https://www.youtube.com/embed/" + movie.youtubeTrailer} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <iframe className="movieTrailer" width="350px" height="315px" src={"https://www.youtube.com/embed/" + movie.youtubeTrailer} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div>
-                    {/* <h1 className="detailedTitle">{movie.title}</h1> */}
                     <div className="detailedRightContainer">
                         <h1 className="detailedTitle">{movie.title}</h1>
                         <div className="detailedInfo">
                             <p className="detailedDesc">{movie.description}</p>
-                            <p className="detailedLength infoPart">Length: <br /> {calculatingTime(movie.length)}</p>
+                            <p className="detailedLength infoPart">Längd: <br /> {calculatingTime(movie.length)}</p>
                             <p className="detailedGenre infoPart">Genre: <br /> {movie.genre}</p>
                             <div className="clearBoth"></div>
-                            <p className="detailedReleaseDate infoPart">Released: <br /> {movie.productionYear}</p>
-                            <p className="detailedDirector infoPart">Director: <br /> {movie.director}</p>
+                            <p className="detailedReleaseDate infoPart">Premiär: <br /> {movie.productionYear}</p>
+                            <p className="detailedDirector infoPart">Regi: <br /> {movie.director}</p>
                             <div className="clearBoth"></div>
                             <div className='allActorsList'>
-                                <p className="detailedActors">Actors: <br /> </p>
+                                <p className="detailedActors">Skådespelare: <br /> </p>
                                 <ul>
                                     {movie.actors.map(actor =>
                                         <li>{actor}</li>
@@ -42,9 +50,9 @@ export default function DetailedInfo() {
                         </div>
 
                         <div className="buttonsUnderText">
-                            <button name='btnRatings' className='buttonRatings' onClick={() => setIsOpen(!isOpen)}>Show Ratings </button>
-                            <button name='screenings' className="buttonScreenings" onClick={() => alert('Screening coming soon')}>View Screenings</button>
-                            {isOpen && (
+                        <button name="btnRatings" className="buttonRatings" onClick={() => setShowRatings(!showRatings)}>Show Ratings</button>
+                            <button name="btnScreenings" className="buttonScreenings" onClick={() => setShowScreenings(!showScreenings)}>View Screenings</button>
+                            {showRatings && (
                                 <div className="detailedRatingDropdown">
                                     {movie.reviews.map(rate =>
                                         <div className="detailedRating">
@@ -61,9 +69,14 @@ export default function DetailedInfo() {
                                     )}
                                 </div>
                             )}
-
+                            {showScreenings && (
+                                 <div className="screeningsDropdown">
+                                <div className="detailedScreeningsDropdown">
+                                    {screenings}
+                                </div>
+                                )}
                         </div>
-
+                        
                     </div>
                 </div>
                 : null}
