c73de0cec91eb43d5609af157db5a7f71ff50202
zompes <simonisvedala@hotmail.com>
2023-02-15 13:27:35 +0100

    fixed comments from Armin

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,30 +1,44 @@
 export default function About() {
     return <>
         <div className="OmOss">
-            <h2>Om oss</h2>
+            <h2>Om Oss</h2>
             <img src="images/Om.jpg" alt="Here will be a image" />
+        </div>
+
+        <div className="varumarkeslofte">
             <p>Vårt varumärkeslöfte, <b>We Make Movies Better</b>,
                 innebär att vi alltid strävar efter att erbjuda de mest minnesvärda och omvälvande  upplevelserna i biografer i världsklass.</p>
+        </div>
 
+        <div className="bioInfo">
             <p>Vi är stolta över att göra det genom ett förstklassigt bemötande från vår personal,
                 stor duk, bekväma VIP-fåtöljer med maximal komfort tillsammans med den bästa tekniken för ljud och bild
                 genom IMAX bild och Prophons ljud biosystem levererar vi den bästa bio upplevensen i världen. Båda våra salonger har det bästa av det bästa i allt,
                 dessutom utvecklar vi löpande våra digitala plattformar där man enkelt kan finna inspiration och information.</p>
+        </div>
 
+        <div className="karriarmojligheter">
             <p>Filmvisarna som arbetsgivare erbjuder sina medarbetare karriärsmöjligheter och en arbetsmiljö som inspirerar.
                 Vi är övertygade om att en förutsättning för att höja besöket på våra biografer är ett team som känner
                 passion för film och brinner för att leverera den bästa möjliga upplevelsen till våra gäster.</p>
+        </div>
 
+        <div className="telefon">
             <p>Vår biosalong kan hittas på denna adress Stortorget 11, 124 84 Småstad, Sverige, behöver du kontakta oss kan du ringa telefonnumret
                 (073) 337 89 01 eller skicka ett email till info@filmvisarna.com. Biografen öppnar alltid 30 minuter innan första föreställning.
                 Öppettiderna kan ändras vid specialevenemang, helgdagar eller skollov.</p>
+        </div>
 
+        <div className="midsommar">
             <p>Midsommarafton och julafton har vi stängt, men dagen efter står vi alltid redo att välkomna biobesökare.</p>
+        </div>
 
+        <div className="regler">
             <p>Filmvisarna tillåter inte att man tar med egen dryck, mat, snacks, godis, popcorn osv in i biografen.
                 Mat, dryck, snacks och godis kan köpas från receptionen, det kommer med stora mängder tack vare vår
                 partnerskap med Snacksjätten. Det finns något för alla, det är tillåtet att ta med egen snacks/dryck om
                 du har hälsomässiga skäl för det</p>
         </div>
+
     </>;
 }
