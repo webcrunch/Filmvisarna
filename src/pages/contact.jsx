1c13051c59067797cd5148cf7dac4bd4e9c92207
harisha7 <harisha.srinivas74@gmail.com>
2023-02-20 14:49:48 +0100

    fixed merge conflict and added bg color.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -20,7 +20,7 @@ export default function Contact() {
         <p>För biljettbokning och övriga ärenden, vänd dig i första hand till någon av biograferna.</p>
         <p>För att kontakta någon av personerna nedan skicka e-post till: förnamn@filmvisarna.com</p>
         <p>För frågor angående programsättning och uthyrningar skicka e-post till: program@filmvisarna.com</p>
-        <h4><b>Thomas Frank</b></h4>
+        <h4 className="contactPerson">Thomas Frank</h4>
         <p>Biografansvarig, kontaktperson </p>
         <p className="E-post">E-post: <a href="info@filmvisarna.com">thomas@filmvisarna.com</a></p>
         <p className="Hemsida">Hemsida: <a href="www.filmvisarna.com">www.filmvisarna.com</a></p>
