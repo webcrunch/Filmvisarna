1f73609b9476217154c24e205d60b86125623e20
harisha7 <harisha.srinivas74@gmail.com>
2023-02-20 14:35:42 +0100

    Added useEffect on register page.

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -11,12 +11,22 @@ function RegisterPage(){
 		password:""
 	})
 
+
+	useEffect(() => {
+		document.body.classList.add("registerPage");
+		return() => document.body.classList.remove("registerPage");
+		
+	}, [] );
+
 	function handleSubmit(e){
 		e.preventDefault();
 		console.log(form.username,form.firstname,form.lastname,form.password);
 	}
 
 	return <>
+
+	
+	
 
 
 		<div className='register'>
@@ -40,4 +50,3 @@ function RegisterPage(){
     </>
 
 }
-
