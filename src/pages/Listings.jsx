aa7a75a2001379124f9529086173f740138225a2
jarl Lindquist <jarllinduist@gmail.com>
2023-02-26 09:48:52 +0100

    adding more information ad set right price(swedish)

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,20 +1,32 @@
 import { useStates } from "../utilities/states";
 import { useEffect } from "react";
 import { calculatingTime } from "../utilities/length-calculating";
+import { get, post, del } from '../utilities/backend-talk';
+
 export default function Listing() {
     const user = useStates('user');
     const { movies, screenings } = useStates('main');
     let bookedArray = [];
-    
-    let userData = user.users.users != undefined ? user.users.users.find(userCheck => userCheck.id === user.id) : null;
-    if (userData) {
-        userData.bookings.map(booked => bookedArray.push(screenings.find(screen => screen.id === booked.id)));
+
+
+    async function getData(user) {
+        let userWithBookings = await get('/api/user/' + user.id);
+        userWithBookings.bookings.map(book => {
+            let screening = screenings.find(screen => screen.id === book.id)
+            bookedArray.push([screening, book]);
+            
+            
+        });
         bookedArray.sort((a, b) => b.Datum - a.Datum)
     }
 
     useEffect(() => {
+        getData(user); 
+            // userData.bookings.map(booked => bookedArray.push(screenings.find(screen => screen.id === booked.id)));
+        // bookedArray.sort((a, b) => b.Datum - a.Datum)
+        
         document.body.classList.add("listings");
-        return () => document.body.classList.remove("listings");
+            return () => document.body.classList.remove("listings");
   }, []);
 
     
@@ -24,7 +36,7 @@ export default function Listing() {
      }
           
 
-    return userData && user.loggedin && (
+    return user.loggedin && (
         <div className="listings">
             {
                 bookedArray.map(p => <ul>
