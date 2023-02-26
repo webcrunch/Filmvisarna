cbe587c7480d4907d75b0a8bbe3f0276743ddc95
harisha7 <harisha.srinivas74@gmail.com>
2023-02-26 15:42:53 +0100

    Corrected names from english to swedish in the register page.

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -5,10 +5,10 @@ export default RegisterPage;
 function RegisterPage(){
 	// const user = useStates('user');
 	const form = useStates({
-		username:"",
-		firstname:"",
-		lastname:"",
-		password:""
+		användarnamn:"",
+		förnamn:"",
+		efternamn:"",
+		lösenord:""
 	})
 
 
@@ -20,7 +20,7 @@ function RegisterPage(){
 
 	function handleSubmit(e){
 		e.preventDefault();
-		console.log(form.username,form.firstname,form.lastname,form.password);
+		console.log(form.användarnamn,form.förnamn,form.efternamn,form.lösenord);
 	}
 
 	return <>
@@ -32,15 +32,15 @@ function RegisterPage(){
 		<div className='register'>
 			<div className='register_form'>
 				<form onSubmit={handleSubmit}>
-					<label className='label'>Username</label>
-					<input type="text" className='username' id="username" name="username" {...form.bind('username')} pattern='[A-Za-z0-9]{7,}' title='Minimum 7 characters required' required />
-					<label className='label'>Password</label>
-					<input className='password' name="password" pattern="[A-Za-z0-9]{8,}" {...form.bind('password')} title='Minimum 8 characters required' required />
-					<label className='label'>Firstname</label>
-					<input className='firstname' type="text" id="firstname" name="firstname" {...form.bind('firstname')} pattern='[A-Za-z0-9]{2,}' title='Minimum 1 characters required' required />
-					<label className='label'>Lastname</label>
-					<input className='lastname' type="text" id="lastname" name="lastname" {...form.bind('lastname')} pattern='[A-Za-z0-9]{2,}' title='Minimum 1 characters required' required />
-					<a href="login.html" className='loginlink'>Already registered? Log In here!</a>
+					<label className='label'>Användarnamn</label>
+					<input type="text" className='användarnamn' id="användarnamn" name="användarnamn" {...form.bind('användarnamn')} pattern='[A-Za-z0-9]{7,}' title='Minimum 7 characters required' required />
+					<label className='label'>Lösenord</label>
+					<input className='lösenord' name="lösenord" pattern="[A-Za-z0-9]{8,}" {...form.bind('lösenord')} title='Minimum 8 characters required' required />
+					<label className='label'>Förnamn</label>
+					<input className='förnamn' type="text" id="förnamn" name="förnamn" {...form.bind('förnamn')} pattern='[A-Za-z0-9]{2,}' title='Minimum 1 characters required' required />
+					<label className='label'>Efternamn</label>
+					<input className='efternamn' type="text" id="efternamn" name="efternamn" {...form.bind('efternamn')} pattern='[A-Za-z0-9]{2,}' title='Minimum 1 characters required' required />
+					<a href="login.html" className='loginlink'>Är du redan registrerad? Logga in här!</a>
 
 					<button type="submit"> Register </button>
 				</form>
