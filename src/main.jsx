b3529ef69fd458a0153ad2cb0578878431b4ba02
jarl Lindquist <jarllinduist@gmail.com>
2023-02-27 17:32:18 +0100

    remove double calls

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App';
 
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 
 );    
