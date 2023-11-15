51b8541fb704a9e0aa26eab2f958f03fa5713bb4
webcrunch <jarllinduist@gmail.com>
2023-11-15 10:17:06 +0100

    adding information to the display of movies booked from a user.

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -3,13 +3,16 @@ import { useEffect } from "react";
 import { calculatingTime } from "../utilities/length-calculating";
 export default function Listing() {
     const user = useStates('user');
-    const { movies, screenings } = useStates('main');
+    const { movies, bookings } = useStates('main');
     let bookedArray = [];
-    
+    let placesArray = [];
     let userData = user.users.users != undefined ? user.users.users.find(userCheck => userCheck.id === user.id) : null;
     if (userData) {
-        userData.bookings.map(booked => bookedArray.push(screenings.find(screen => screen.id === booked.id)));
+        userData.bookings.map(booked => bookedArray.push(bookings.find(book => book.id === booked.code)));
+        console.log(bookedArray);
         bookedArray.sort((a, b) => b.Datum - a.Datum)
+        // chairHandling
+        
     }
 
     useEffect(() => {
@@ -19,6 +22,15 @@ export default function Listing() {
 
     const getPrice = i => userData.bookings[i].price + "kr";
 
+    const chairHandling = chairs => {
+        placesArray.length > 0 ? placesArray = []: null;
+        for (const [key, value] of Object.entries(chairs)) {
+            let row = key.split(" ")[0];
+            let chair = key.split(" ")[1];
+            placesArray.push({ row: row, chair: chair })
+            placesArray.sort();
+        }
+    }
     
     function getMovies(name, properies) {
         let movie = movies.find(movie => movie.title == name);
@@ -29,13 +41,32 @@ export default function Listing() {
     return userData && user.loggedin && (
         <div className="listings">
             {
-                bookedArray.map((p,i) => <ul>
-                    <li>Film: {p.film}</li>
-                    <li>Längd:{" "} { calculatingTime(getMovies(p.film, 'length'))}</li>
-                    <li>Datum: {p.date}</li>
+                bookedArray.map( p => <ul>
+                    <li>Bokningskod: {p.id}</li>
+                    <li>Film: {p.screeningsData.film}</li>
+                    <li>Längd:{" "} { calculatingTime(getMovies(p.screeningsData.film, 'length'))}</li>
+                    <li>Datum: {p.screeningsData.date}</li>
+                    <li>Tid: {p.screeningsData.time}</li>
                     <li>Sal: {p.auditorium}</li>
-                    <li>Tid: {p.time}</li>
-                    <li>Pris: {getPrice(i)}</li>
+                    <li>Pris: {p.totalPrice} Kr</li> 
+                    <li>Antal stolar: {p.totalSeats}</li>
+                    {
+                        p.numberofAdults > 0 ? <li>Antal Vuxna: {p.numberofAdults} </li>: null    
+                    } 
+                    {
+                       p.numberofSenior > 0 ? <li>Antal Pensionärer: {p.numberofSenior} </li>: null
+                    } 
+                    {
+                       p.priceChildren > 0 ? <li>Antal Barn: {p.numberofChildren} </li>: null
+                    } 
+                    {chairHandling(p.markedChairs)}
+                    <li>Platser:</li>
+                    <ul>
+                    {
+                        placesArray.map(place => <li>Rad:{place.row} Stolrad: {place.chair}</li>)
+                    }
+                    </ul>
+                    <br />
                 </ul>)
             }
             </div>
