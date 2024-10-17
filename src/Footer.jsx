7f03c281eb104fcf37490744533ab67a259aec39
draginojd <arminfazlikhan@gmail.com>
2024-10-17 19:11:21 +0200

    seat overflow issues fixed for phones

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,20 +1,20 @@
 export default function Footer() {
   return <>
     <footer>
-      <div className="footerleft" >
+      <div className="footer" >
       <h3>Information:</h3>
       <p className="adress">Adress: Stortorget 11, 124 84 Småstad, Sverige</p>
       <p className="phonenumber"><a href="tel:+4733378901">Telefon: (073) 337 89 01 </a></p>
       <p className="email">Epost <a href="info@filmvisarna.com">info@filmvisarna.com</a></p>
      </div>
       
-      <div className="footercenter">
+      <div className="footer">
         <h3 className="openinghtitle">Öppetider:</h3>
         <p className="hourweekdays">Mån-Fre: 15:00-22:00</p>
         <p className="hourweekend">Lör-Sön: 15:00-23:00</p>
       </div>
 
-      <div className="footerright">
+      <div className="footer">
         <h3>Socials</h3>
           <a href="#"><img src="./images/facebook.png" alt=""facebookalt="true" width="50px" height="50px"></img></a>
           <a href="#"><img src="./images/instagram.png" alt=""facebookalt="true" width="50px" height="50px"></img></a>
